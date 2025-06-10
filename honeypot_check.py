4acc1303907859c1548f25ac1836593057d93ec8
fvl221 <fdiop9111@gmail.com>
2025-06-10 09:56:37 +0200

    Create honeypot_check.py

---
+++ honeypot_check.py
@@ -0,0 +1,5 @@
+import random
+
+def is_safe_token(token):
+    # À remplacer plus tard par une API honeypot réelle (ex: honeypot.is)
+    return random.choice([True, True, True, False])  # 75% safe pour test
